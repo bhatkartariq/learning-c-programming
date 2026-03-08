bfe630ea155d54ced90c5ae367ce932a3e925732
Tariq bhatkar <bhatkartariq@eng.rizvi.edu.in>
2026-03-08 15:54:42 +0530

    Add files via upload

---
+++ char_int_float.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int age =25;
+    int num=35;//int is used for saving integers
+    char name="Tariq";//char used to save characters
+    float pi= 3.14;//float is used for saving decimal numbers
+    //giving variable name cannot start with number or signs 
+    //it can only start by '_' or any alphabet 
+    //its case sensitive if you are saving A =30 and a =79 
+    // both are saved seprately
+    //while saving var name you can only use '_'
+    //no other signs can be used !!!!
+    //example below
+    int a=79;
+    int A=9090;
+    age=35;//age can be updated later on 
+    //as a good programmer you should alaways give the variable name 
+    //that is actually saying what is saved in it
+    //example int age = 25 ; it shows that age is saved here 
+    return 0;
+}
---
+++ introduction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>//library to start with 
+//without include stdio you cant run the code its mandatory !!
+//int main is the main program
+//it is mandatory to use 
+//it stores the main data and the main program
+//after most of the lines we need to use semi colon ';'
+// it is super important most of the times the code dont run 
+//because the semicolon was not there 
+//please always check it whenever it is necessary
+int main(){
+    printf("hello world");//used to print on screen whatever you want
+    return 0;
+    //return is mainly used to end code or return the value to 0
+    //must be written most of the time , and 
+    //we can actually show that here the code ends
+    // or one point of code ends
+}
