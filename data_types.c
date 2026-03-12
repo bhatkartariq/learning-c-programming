6bbe3aafbc45ab73f1b762ba876e7fc21978fbe8
Tariq bhatkar <bhatkartariq@eng.rizvi.edu.in>
2026-03-12 21:43:47 +0530

    Document data types and examples in data_types.c
    Added comments explaining data types and their sizes.

--- data_types.c
+++ data_types.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
-
+/*DATA TYPES AND THEIR SIZES IN BYTES
+----ALL ARE IN BYTES----
+char / signed char / unsigned char --1 
+int / signed int / unsigned int -- 2
+short / signed / unsigned short int --2
+long / signed long / unsigned long int --4
+float -- 4
+double --8
+lonng double --10
+*/
 int main(){
+    int age =22;
+    //here int is the data type and age is the variable in which 
+    //we save the data (basically the name of the data )
+    //A GOOD PROGRAMMER ALWAYS SAVES THE NAME THAT IS RELATABLE
+    //TO WHAT THE DATA IS ABOUT 
+    char words='hello';
+    //similarly here the char is the data type 
+    // words is the name we saved the char as
+    //'' is used to save the strings
+    //hello is the word that is saved
+    float pi=3.14;
+    // here float is the data type 
+    //the name that is saved is pi
+    // and the value is 3.14
+
+
 
 }
